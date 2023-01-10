2346060c9fb868f3c88d71e01119b49ab003ea3a
sukhyunmoon <sukhyunmoon@sk.com>
2023-01-10 16:25:04 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          AWS 첫 배포를 축하한다. RYAN
+          AWS 첫 배포를 축하한다. RYAN!
         </h1>
 
         <p className={styles.description}>
